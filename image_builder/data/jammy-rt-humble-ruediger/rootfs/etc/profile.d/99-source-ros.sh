edf6345fdabef00ff3280391cafa7e040500acdc
Florent Chretien <flopubs@gmail.com>
2023-12-06 12:21:46 +0600

    update the setup.bash to local_setup.bash

--- image_builder/data/jammy-rt-humble-ruediger/rootfs/etc/profile.d/99-source-ros.sh
+++ image_builder/data/jammy-rt-humble-ruediger/rootfs/etc/profile.d/99-source-ros.sh
@@ -2,8 +2,8 @@ if [ -f /opt/ros/humble/setup.bash ]; then
   source /opt/ros/humble/setup.bash
 fi
 if [ -f /opt/ruediger2/ruediger2_control/setup.bash ]; then
-  source /opt/ruediger2/ruediger2_control/setup.bash
+  source /opt/ruediger2/ruediger2_control/local_setup.bash
 fi
 if [ -f /opt/ruediger_ws/setup.bash ]; then
-  source /opt/ruediger_ws/setup.bash
+  source /opt/ruediger_ws/local_setup.bash
 fi
--- image_builder/data/jammy-rt-humble-stanley/rootfs/etc/profile.d/99-source-ros.sh
+++ image_builder/data/jammy-rt-humble-stanley/rootfs/etc/profile.d/99-source-ros.sh
@@ -2,8 +2,8 @@ if [ -f /opt/ros/humble/setup.bash ]; then
   source /opt/ros/humble/setup.bash
 fi
 if [ -f /opt/stanley2/stanley2_control/setup.bash ]; then
-  source /opt/stanley2/stanley2_control/setup.bash
+  source /opt/stanley2/stanley2_control/local_setup.bash
 fi
 if [ -f /opt/stanley_ws/setup.bash ]; then
-  source /opt/stanley_ws/setup.bash
+  source /opt/stanley_ws/local_setup.bash
 fi
